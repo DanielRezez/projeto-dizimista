4ed19b1b25014dcfc337b8dfd78267c4f2932969
DanielRezez <danielrezes@outlook.com>
2025-01-07 17:02:37 -0300

    atributos do models.py do dizimistas feito - integracao com o banco de dados

--- back_end/dizimistas/models.py
+++ back_end/dizimistas/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
 
 # Create your models here.
+class Dizimista(models.Model):
+    id_paroquia = models.IntegerField()
+    ficha = models.IntegerField()
+    nome = models.CharField(max_length=255)
+    data_nascimento = models.DateField()
+    email = models.EmailField(blank=True, null=True)
+    telefone = PhoneNumberField(blank=True, null=True),
+    situacao = models.CharField(max_length=1, choices=[('A', 'Ativo'), ('I', 'Inativo')], default='A')
+    
+    def __str__(self):
+        return self.nome
+    
+    class Meta:
+        unique_together = ('id_paroquia', 'ficha')
+        db_table='tb_dizimistas'
+        permissions=[
+            ('can_view_dizimista', 'Can view dizimista'),
+            ('can_edit_dizimista', 'Can edit dizimista'),
+        ]
